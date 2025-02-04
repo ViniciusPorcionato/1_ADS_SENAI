8ef8fb376b97434636bc0e8801563d4ffb3a89cf
Vinicius Porcionato <porcionatovinicius@gmail.com>
2025-02-03 22:34:34 -0300

    Inicio da ciencia de dados com python

---
+++ CienciasDeDados/MyFirstProject/myfourthproject.py
@@ -0,0 +1,14 @@
+# entrada de dados
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+nota3 = float(input("Digite a terceira nota:"))
+
+# processamento
+media = (nota1 + nota2 + nota3) / 3
+
+# saida
+print(f"Primeira nota : {nota1} \n"
+      f"Segunda nota : {nota2} \n"
+      f"Terceira nota : {nota3} \n"
+      f"Média: {media} \n"
+      )
---
+++ CienciasDeDados/MyFirstProject/mythirdproject.py
@@ -0,0 +1,13 @@
+a = float(input("Digite o primeiro valor :"))
+b = float(input("Digite o segundo valor :"))
+
+soma = a + b
+subtracao = a - b
+multiplicaca = a * b
+divisao = a / b
+
+
+print(f"O valor da soma é: {soma}")
+print(f"O valor da subtracao é: {subtracao}")
+print(f"O valor da multiplicaca é: {multiplicaca}")
+print(f"O valor da divisao é: {divisao}")
